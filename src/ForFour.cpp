8927cdf6d263d6252a5e281cd0da5272ddae3968
Domino Marama <domino@dominodesigns.info>
2019-12-07 14:50:31 +0000

    Whitespace cleanup

--- src/ForFour.cpp
+++ src/ForFour.cpp
@@ -2,50 +2,51 @@
 
 
 struct ReTunes_ForFour : Module {
-	enum ParamIds {
-		COUNT1_PARAM,
-		COUNT2_PARAM,
-		COUNT3_PARAM,
-		NUM_PARAMS
-	};
-	enum InputIds {
-		CLOCK_INPUT,
-		RESET_INPUT,
-		NUM_INPUTS
-	};
-	enum OutputIds {
-                // BEAT1 to BEAT4 must be first four outputs
-		BEAT1_OUTPUT,
-		BEAT2_OUTPUT,
-		BEAT3_OUTPUT,
-		BEAT4_OUTPUT,
-		EVENBAR_OUTPUT,
-		FILLBARS_OUTPUT,
-		COUNTER1_OUTPUT,
-		COUNTER2_OUTPUT,
-		COUNTER3_OUTPUT,
-		NUM_OUTPUTS
-	};
-	enum LightIds {
-		NUM_LIGHTS
-	};
+    enum ParamIds {
+        COUNT1_PARAM,
+        COUNT2_PARAM,
+        COUNT3_PARAM,
+        NUM_PARAMS
+    };
+    enum InputIds {
+        CLOCK_INPUT,
+        RESET_INPUT,
+        NUM_INPUTS
+    };
+    enum OutputIds {
+        // BEAT1 to BEAT4 must be first four outputs
+        BEAT1_OUTPUT,
+        BEAT2_OUTPUT,
+        BEAT3_OUTPUT,
+        BEAT4_OUTPUT,
+        EVENBAR_OUTPUT,
+        FILLBARS_OUTPUT,
+        COUNTER1_OUTPUT,
+        COUNTER2_OUTPUT,
+        COUNTER3_OUTPUT,
+        NUM_OUTPUTS
+    };
+    enum LightIds {
+        NUM_LIGHTS
+    };
 
-        ReTunes_ForFour() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-                configParam(COUNT1_PARAM, 1.f, 256.f, 8.f, "Count 1");
-                configParam(COUNT2_PARAM, 1.f, 256.f, 16.f, "Count 2");
-                configParam(COUNT3_PARAM, 1.f, 256.f, 32.f, "Count 3");
-                onReset();
-        };
+    ReTunes_ForFour() {
+        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+        configParam(COUNT1_PARAM, 1.f, 256.f, 8.f, "Count 1");
+        configParam(COUNT2_PARAM, 1.f, 256.f, 16.f, "Count 2");
+        configParam(COUNT3_PARAM, 1.f, 256.f, 32.f, "Count 3");
+        onReset();
+    };
 
-        void process(const ProcessArgs& args) override;
+    void process(const ProcessArgs& args) override;
 
-        int counter, beat, countdown1, countdown2, countdown3;
+    int counter, beat, countdown1, countdown2, countdown3;
 
-        bool resetNext = true;
+    bool resetNext = true;
 
-        dsp::SchmittTrigger clockTrigger, resetTrigger;
+    dsp::SchmittTrigger clockTrigger, resetTrigger;
 };
+
 
 void ReTunes_ForFour::process(const ProcessArgs &args) {
     if (resetTrigger.process(inputs[RESET_INPUT].value)) {
@@ -88,14 +89,14 @@ void ReTunes_ForFour::process(const ProcessArgs &args) {
 
 
 struct ReTunes_ForFourWidget : ModuleWidget {
-        ReTunes_ForFourWidget(ReTunes_ForFour* module) {
-		setModule(module);
-                setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/ReTunes-ForFour.svg")));
+    ReTunes_ForFourWidget(ReTunes_ForFour* module) {
+        setModule(module);
+        setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/ReTunes-ForFour.svg")));
 
         addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
         addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
         {
             auto w = createParamCentered<RoundBlackKnob>(mm2px(Vec(9.131, 89.435)), module, ReTunes_ForFour::COUNT1_PARAM);
@@ -116,16 +117,16 @@ struct ReTunes_ForFourWidget : ModuleWidget {
         addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.544, 22.292)), module, ReTunes_ForFour::CLOCK_INPUT));
         addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.544, 40.087)), module, ReTunes_ForFour::RESET_INPUT));
 
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 17.0)), module, ReTunes_ForFour::BEAT1_OUTPUT));
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 29.073)), module, ReTunes_ForFour::BEAT2_OUTPUT));
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 41.145)), module, ReTunes_ForFour::BEAT3_OUTPUT));
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 53.218)), module, ReTunes_ForFour::BEAT4_OUTPUT));
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 65.29)), module, ReTunes_ForFour::EVENBAR_OUTPUT));
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 77.363)), module, ReTunes_ForFour::FILLBARS_OUTPUT));
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 89.435)), module, ReTunes_ForFour::COUNTER1_OUTPUT));
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 101.507)), module, ReTunes_ForFour::COUNTER2_OUTPUT));
-                addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 113.58)), module, ReTunes_ForFour::COUNTER3_OUTPUT));
-	}
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 17.0)), module, ReTunes_ForFour::BEAT1_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 29.073)), module, ReTunes_ForFour::BEAT2_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 41.145)), module, ReTunes_ForFour::BEAT3_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 53.218)), module, ReTunes_ForFour::BEAT4_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 65.29)), module, ReTunes_ForFour::EVENBAR_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 77.363)), module, ReTunes_ForFour::FILLBARS_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 89.435)), module, ReTunes_ForFour::COUNTER1_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 101.507)), module, ReTunes_ForFour::COUNTER2_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.936, 113.58)), module, ReTunes_ForFour::COUNTER3_OUTPUT));
+    }
 };
 
 
